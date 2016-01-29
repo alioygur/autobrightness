991039599bf0c8e1dd0c74655a1bbc20f8746ab1
Ali OYGUR <alioygur@gmail.com>
2016-01-29 19:02:32 +0200

    updated path

--- autobrightness.sh
+++ autobrightness.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 H=`date +"%H"`
 MORNING=9
 EVENING=17
 
 if [[ H -gt MORNING && H -lt EVENING ]]; then
-  xbacklight -set 90 -d :0
+  /usr/bin/xbacklight -set 90 -d :0
 else
-  xbacklight -set 25 -d :0
+  /usr/bin/xbacklight -set 25 -d :0
 fi
